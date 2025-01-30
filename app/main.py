8dc65dd44617364cd7ea2511e0188f6c1273125f
Ngayka <ngayka@gmail.com>
2025-01-30 22:09:10 +0200

    should to finalize the output message

--- app/main.py
+++ app/main.py
@@ -1,1 +1,36 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name, health=100, hidden=False):
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+
+        Animal.alive.append(self)
+
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health <= 0 and self in Animal.alive:
+            Animal.alive.remove(self)
+
+    def __repr__(self):
+        return f'{{"Name": "{self.name}", "Health": {self.health}, "Hidden": {self.hidden}}}'
+
+class Herbivore(Animal):
+    def hide(self):
+        self.hidden = not self.hidden
+        return self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, food):
+        if isinstance(food, Carnivore):
+            print(f"{self.name} can not attacked {food.name} because it carnivore")
+            return
+
+        if food.hidden:
+            print(f"{self.name} can`t attacked {food.name} because it hide")
+            return
+
+        food.take_damage(50)
+        return
