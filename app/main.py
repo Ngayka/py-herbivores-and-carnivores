1c52ec20debfe64144c7031d725f6f9a6f9184fe
Ngayka <ngayka@gmail.com>
2025-01-31 10:05:33 +0200

    solution is ready

--- app/main.py
+++ app/main.py
@@ -1,23 +1,24 @@
 class Animal:
     alive = []
 
-    def __init__(self, name, health=100, hidden=False):
+    def __init__(self, name: str, health=100, hidden=False):
         self.name = name
         self.health = health
         self.hidden = hidden
 
         Animal.alive.append(self)
 
-    def take_damage(self, damage):
+    def take_damage(self, damage) -> list:
         self.health -= damage
         if self.health <= 0 and self in Animal.alive:
             Animal.alive.remove(self)
 
     def __repr__(self):
-        return f'{{"Name": "{self.name}", "Health": {self.health}, "Hidden": {self.hidden}}}'
+        return f'{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}'
+
 
 class Herbivore(Animal):
-    def hide(self):
+    def hide(self) -> bool:
         self.hidden = not self.hidden
         return self.hidden
 
@@ -25,11 +26,13 @@ class Herbivore(Animal):
 class Carnivore(Animal):
     def bite(self, food):
         if isinstance(food, Carnivore):
-            print(f"{self.name} can not attacked {food.name} because it carnivore")
+            print(
+                f"{self.name} can not attacked {food.name} because it`s carnivore"
+            )
             return
 
         if food.hidden:
-            print(f"{self.name} can`t attacked {food.name} because it hide")
+            print(f'{self.name} can`t attacked {food.name} because it`s hide')
             return
 
         food.take_damage(50)
